ce741583c63444ca87d95791edb60b650f16a189
Nathan Keller <keller.nathan@gmail.com>
2020-03-19 20:22:27 -0600

    Most of the CRUD functionality for items. Not sure how to create an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,27 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+   render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  def update
+    render json: Item.update(item_params)
+  end
+
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
+  end
 end
