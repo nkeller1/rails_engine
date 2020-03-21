5c134db60ccedbb44c09de19800b0353b1757d1a
Nathan Keller <keller.nathan@gmail.com>
2020-03-21 17:59:30 -0600

    Install Gem Simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'shoulda/matchers'
+require 'simplecov'
+SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -62,7 +64,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  
+
     Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
