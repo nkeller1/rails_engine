10a6223663dd787ac7969aefa78069e2a9ac7fca
Nathan Keller <keller.nathan@gmail.com>
2020-03-21 12:58:02 -0600

    Single finder tested up until partials

--- app/controllers/api/v1/find_controller.rb
+++ app/controllers/api/v1/find_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::FindController < ApplicationController
-    def index
-      # require "pry"; binding.pry
-      render json: MerchantSerializer.new(Merchant.where(request.GET))
-    end
-
-    # def show
-    #   render json: MerchantSerializer.new(Merchant.where(request.GET))
-    # end
+  def index
+    render json: MerchantSerializer.new(Merchant.where(request.GET))
+  end
 end
