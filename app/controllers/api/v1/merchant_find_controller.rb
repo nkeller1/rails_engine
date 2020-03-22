6e8dc2461b95d7b3a75e10b914fa8b537885ac46
Nathan Keller <keller.nathan@gmail.com>
2020-03-21 18:35:34 -0600

    Routes created.

--- app/controllers/api/v1/merchant_find_controller.rb
+++ app/controllers/api/v1/merchant_find_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::MerchantFindController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.where(request.GET).limit(1))
   end
+
+  def index
+    render json: MerchantSerializer.new(Merchant.where(request.GET))
+  end
 end
