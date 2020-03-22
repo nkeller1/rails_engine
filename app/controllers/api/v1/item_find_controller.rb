eb8a578ddf690436250ed7d7ed82470ffe115c63
Nathan Keller <keller.nathan@gmail.com>
2020-03-21 18:28:40 -0600

    Add limit one to single finders

--- app/controllers/api/v1/item_find_controller.rb
+++ app/controllers/api/v1/item_find_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ItemFindController < ApplicationController
   def show
-    render json: ItemSerializer.new(Item.where(request.GET))
+    render json: ItemSerializer.new(Item.where(request.GET).limit(1))
   end
 end
--- app/controllers/api/v1/merchant_find_controller.rb
+++ app/controllers/api/v1/merchant_find_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MerchantFindController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.where(request.GET))
+    render json: MerchantSerializer.new(Merchant.where(request.GET).limit(1))
   end
 end
